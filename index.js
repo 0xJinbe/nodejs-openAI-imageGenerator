04fb0412894f696f06c6768e890110d6b7d83521
claudio-germano <wantuir.germano@gmail.com>
2022-12-26 21:07:58 -0300

    firstUI build

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -8,7 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded( { extended: false }));
 
-
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
