6385f87a6cb996e6ece27ecdff94445073a2550d
claudio-germano <wantuir.germano@gmail.com>
2022-12-26 23:56:19 -0300

    form event listener; generateImageRequest(); render image;

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -5,7 +5,7 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-const generateImage =async (req, res) => { 
+const generateImage = async (req, res) => { 
     const { prompt, size } = req.body;
 
     const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' :  '1024x1024' ;
